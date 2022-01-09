208dfe8ea30f2a1e91405436605a0e7a4a3a0920
Andrew Njoo <adnjoo@gmail.com>
2022-01-08 22:05:47 -0800

    edit front-end

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,25 @@
-import './App.css';
+import { useEffect, useState } from "react";
+import axios from "axios";
+import "./App.css";
 
 function App() {
+  let [result, setResult] = useState("");
+
+  useEffect(() => {
+    search();
+  }, []);
+
+  //search for artist
+  const search = async () => {
+    const res = await axios.get(`http://localhost:4001`);
+    console.log("results", res.data);
+    setResult(res.data);
+  };
+
   return (
     <div className="App">
-      hi
+      hi here's a :<br />
+      {result}
     </div>
   );
 }
