d0525bd94963a7bdb40a0e2d5d504990a6009259
Andrew Njoo <adnjoo@gmail.com>
2022-01-10 13:44:09 -0800

    add eslint to client

--- README.md
+++ README.md
@@ -4,8 +4,17 @@ This is an inventory tracking web application for a logistics company.
 
 Technologies:
 
+* Next.js
+
 * PostgreSQL
 
 * Node
 
 * Express
+
+
+Data Model:
+
+Inventory item -
+
+Columns: Date created, Name, Description, Quantity, Price, Image URL
---
+++ client/pages/components/Mynavbar.component.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { Navbar, Container } from 'react-bootstrap';
+
+
+export const Mynavbar = () => {
+  return (
+    <div>
+      <Navbar bg="light" variant="light"> 
+        <Container>
+          <Navbar.Brand href="#home">Inventory Tracker</Navbar.Brand>
+          <Navbar.Toggle />
+          <Navbar.Collapse className="justify-content-end">
+            <Navbar.Text>
+              created by: <a href="https://github.com/adnjoo/backend-challenge" target='_blank'>adnjoo</a>
+            </Navbar.Text>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </div>
+  );
+};
