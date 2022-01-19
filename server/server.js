dd0422d3c1bd5ded5cdb4afe9dd141f67f776d1d
Andrew Njoo <adnjoo@gmail.com>
2022-01-18 19:38:39 -0800

    edit

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const cors = require('cors');
 
-const port = process.env.PORT || 4001;
+// const port = process.env.PORT || 4001;
 const controller = require('./controller');
 
 const app = express();
