f10f6d21ee679463e6c106077cf349983800c1b5
Andrew Njoo <adnjoo@gmail.com>
2022-01-18 17:07:22 -0800

    add additional tests

--- client/cypress/integration/nav.spec.js
+++ client/cypress/integration/nav.spec.js
@@ -1,7 +1,19 @@
-describe('Nav Menus', () => {
+describe('Homepage', () => {
   describe('When you visit home', () => {
     it('Should visit home page', () => {
       cy.visit('/');
+    });
+  });
+  describe('When you visit home', () => {
+    it('it should find the content "Inventory Tracker"', () => {
+      cy.visit('/');
+      cy.contains('Inventory Tracker');
+    });
+  });
+  describe('When you visit home', () => {
+    it('it should find the content "Product"', () => {
+      cy.visit('/');
+      cy.contains('Product');
     });
   });
 });
